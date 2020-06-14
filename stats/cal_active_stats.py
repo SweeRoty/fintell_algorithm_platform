ef42633408f12699a1ba33ae36416c243ccc4651
SweeRoty <sweeroty@gmail.com>
2020-06-14 10:40:20 +0800

    add an os parameter, modify itime condition, use approx_count instead of count and create dataframe from rdd

--- stats/cal_active_stats.py
+++ stats/cal_active_stats.py
@@ -4,39 +4,46 @@ from ConfigParser import RawConfigParser
 from datetime import datetime, timedelta
 from pyspark import SparkConf
 from pyspark.sql import functions as F
-from pyspark.sql import SparkSession
+from pyspark.sql import Row, SparkSession
 
 import argparse
 
-def retrieveUidInfo(spark, to):
+def retrieveUidInfo(spark, to, os):
+	device = 'imei' if os == 'a' else 'idfa'
 	sql = """
 		select
 			distinct uid,
 			package_name app_package,
-			imei
+			{0} device_id
 		from
 			edw.user_register_log
 		where
-			data_date <= '{0}'
-	""".format(to)
+			data_date <= '{1}'
+			and platform = '{2}'
+	""".format(device, to, os)
 	print(sql)
 	uids = spark.sql(sql)
 	return uids
 
-def retrieveActiveRecords(spark, fr, to):
+def retrieveActiveRecords(spark, fr, to, os):
 	sql = """
 		select
-			platform os,
 			uid
 		from
 			edw.active_user_log
 		where
 			data_date between '{0}' and '{1}'
-			and itime between unix_timestamp('{0}', 'YYYYmmdd') and unix_timestamp('{1}', 'YYYYmmdd')
-	""".format(fr, to)
+			and from_unixtime(itime, 'yyyyMMdd') between '{0}' and {1}'
+			and platform = '{2}'
+	""".format(fr, to, os)
 	print(sql)
 	records = spark.sql(sql)
 	return records
+
+def transform_to_row(row_dict):
+	global args
+	row_dict['data_date'] = args.fr
+	return Row(**row_dict)
 
 if __name__ == '__main__':
 	print('====> Initializing Spark APP')
@@ -46,7 +53,7 @@ if __name__ == '__main__':
 	for t in localConf.items('spark-config'):
 		sparkConf.set(t[0], t[1])
 	spark = SparkSession.builder \
-			.appName('RLab_Stats_Report___Prepare_Active_Data') \
+			.appName('RLab_Stats_Report___Cal_Active_Stats') \
 			.config(conf=sparkConf) \
 			.enableHiveSupport() \
 			.getOrCreate()
@@ -55,54 +62,41 @@ if __name__ == '__main__':
 
 	print('====> Parsing local arguments')
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--query_date', type=str)
-	parser.add_argument('--forward', type=int)
+	parser.add_argument('--fr', type=str)
+	parser.add_argument('--to', type=str)
+	parser.add_argument('--os', choices=['a', 'i'])
 	args = parser.parse_args()
-	to = datetime.strptime(args.query_date, '%Y%m%d').date()+timedelta(days=args.forward)
-	to = to.strftime('%Y%m%d')
+	assert args.fr <= args.to
 
 	print('====> Start calculation')
 	result = {}
-	uids = retrieveUidInfo(spark, to).repartition(5000, ['uid'])
-	records = retrieveActiveRecords(spark, args.query_date, to).repartition(5000, ['uid'])
+	uids = retrieveUidInfo(spark, args.to, args.os)
+	records = retrieveActiveRecords(spark, args.fr, args.to, args.os)
 	records = records.join(uids, on=['uid'], how='left_outer').cache()
 
-	devices = records.groupBy(['imei', 'os']).agg(F.count(F.lit(1)).alias('device_active_times'), \
-		F.countDistinct('app_package').alias('device_active_app_count')).cache()
-	devices_stats = devices.where(devices.os == 'a').select(F.count(F.lit(1)).alias('android_active_device_count'), \
-		F.mean('device_active_times').alias('android_avg_active_times_pd'), \
-		F.mean('device_active_app_count').alias('android_avg_active_app_pd')).collect()
-	result['android_active_device_count'] = devices_stats[0]['android_active_device_count']
-	result['android_avg_active_times_pd'] = devices_stats[0]['android_avg_active_times_pd']
-	result['android_avg_active_app_pd'] = devices_stats[0]['android_avg_active_app_pd']
-	'''
-	devices_stats = devices.where(devices.os == 'i').select(F.count(F.lit(1)).alias('ios_active_device_count'), \
-		F.mean('device_active_times').alias('ios_avg_active_times_pd'), \
-		F.mean('device_active_app_count').alias('ios_avg_active_app_pd')).collect()
-	result['ios_active_device_count'] = devices_stats[0]['ios_active_device_count']
-	result['ios_avg_active_times_pd'] = devices_stats[0]['ios_avg_active_times_pd']
-	result['ios_avg_active_app_pd'] = devices_stats[0]['ios_avg_active_app_pd']
-	'''
-	devices.unpersist()
+	devices = records.repartition(5000, ['device_id']).groupBy(['device_id']).agg(\
+		F.count(F.lit(1)).alias('device_active_times'), \
+		F.approx_count_distinct('app_package', rsd=0.05).alias('device_active_app_count'))
+	devices = devices.select(\
+		F.count(F.lit(1)).alias('active_device_count'), \
+		F.mean('device_active_times').alias('avg_active_times_per_device'), \
+		F.mean('device_active_app_count').alias('avg_active_app_per_device')).collect()
+	#print(devices)
+	result['active_device_count'] = devices[0]['active_device_count']
+	result['avg_active_times_per_device'] = devices[0]['avg_active_times_per_device']
+	result['avg_active_app_per_device'] = devices[0]['avg_active_app_per_device']
 
-	apps = records.groupBy(['app_package', 'os']).agg(F.count(F.lit(1)).alias('app_active_times'), \
-		F.countDistinct('imei').alias('app_active_device_count')).cache()
-	apps_stats = apps.where(apps.os == 'a').select(F.count(F.lit(1)).alias('android_active_app_count'), \
-		F.mean('app_active_times').alias('android_avg_active_times_pa'), \
-		F.mean('app_active_device_count').alias('android_avg_active_device_pa')).collect()
-	result['android_active_app_count'] = apps_stats[0]['android_active_app_count']
-	result['android_avg_active_times_pa'] = apps_stats[0]['android_avg_active_times_pa']
-	result['android_avg_active_device_pa'] = apps_stats[0]['android_avg_active_device_pa']
-	'''
-	apps_stats = apps.where(apps.os == 'i').select(F.count(F.lit(1)).alias('ios_active_app_count'), \
-		F.mean('app_active_times').alias('ios_avg_active_times_pa'), \
-		F.mean('app_active_device_count').alias('ios_avg_active_device_pa')).collect()
-	result['ios_active_app_count'] = apps_stats[0]['ios_active_app_count']
-	result['ios_avg_active_times_pa'] = apps_stats[0]['ios_avg_active_times_pa']
-	result['ios_avg_active_device_pa'] = apps_stats[0]['ios_avg_active_device_pa']
-	'''
-	apps.unpersist()
+	apps = records.repartition(5000, ['app_package']).groupBy(['app_package']).agg(\
+		F.count(F.lit(1)).alias('app_active_times'), \
+		F.approx_count_distinct('device_id', rsd=0.05).alias('app_active_device_count'))
+	apps = apps.select(\
+		F.count(F.lit(1)).alias('active_app_count'), \
+		F.mean('app_active_times').alias('avg_active_times_per_app'), \
+		F.mean('app_active_device_count').alias('avg_active_device_per_app')).collect()
+	#print(apps)
+	result['active_app_count'] = apps_stats[0]['active_app_count']
+	result['avg_active_times_per_app'] = apps_stats[0]['avg_active_times_per_app']
+	result['avg_active_device_per_app'] = apps_stats[0]['avg_active_device_per_app']
 
-	result = spark.createDataFrame([result])
-	result = result.withColumn('data_date', F.lit(args.query_date))
-	result.repartition(1).write.csv('/user/hive/warehouse/ronghui.db/rlab_stats_report/active/{0}'.format(args.query_date), header=True)
+	result = sc.parallelize([result]).map(transform_to_row).toDF()
+	result.repartition(1).write.csv('/user/hive/warehouse/ronghui.db/rlab_stats_report/active/{0}'.format(args.fr), header=True)
--- stats/cal_online_stats.py
+++ stats/cal_online_stats.py
@@ -4,39 +4,46 @@ from ConfigParser import RawConfigParser
 from datetime import datetime, timedelta
 from pyspark import SparkConf
 from pyspark.sql import functions as F
-from pyspark.sql import SparkSession
+from pyspark.sql import Row, SparkSession
 
 import argparse
 
-def retrieveUidInfo(spark, to):
+def retrieveUidInfo(spark, to, os):
+	device = 'imei' if os == 'a' else 'idfa'
 	sql = """
 		select
 			distinct uid,
 			package_name app_package,
-			imei
+			{0} device_id
 		from
 			edw.user_register_log
 		where
-			data_date <= '{0}'
-	""".format(to)
+			data_date <= '{1}'
+			and platform = '{2}'
+	""".format(device, to, os)
 	print(sql)
 	uids = spark.sql(sql)
 	return uids
 
-def retrieveOnlineRecords(spark, fr, to):
+def retrieveOnlineRecords(spark, fr, to, os):
 	sql = """
 		select
-			platform os,
 			uid
 		from
 			edw.online_user_log
 		where
 			data_date between '{0}' and '{1}'
-			and itime between unix_timestamp('{0}', 'YYYYmmdd') and unix_timestamp('{1}', 'YYYYmmdd')
-	""".format(fr, to)
+			and from_unixtime(itime, 'yyyyMMdd') between '{0}' and {1}'
+			and platform = '{2}'
+	""".format(fr, to, os)
 	print(sql)
 	records = spark.sql(sql)
 	return records
+
+def transform_to_row(row_dict):
+	global args
+	row_dict['data_date'] = args.fr
+	return Row(**row_dict)
 
 if __name__ == '__main__':
 	print('====> Initializing Spark APP')
@@ -46,7 +53,7 @@ if __name__ == '__main__':
 	for t in localConf.items('spark-config'):
 		sparkConf.set(t[0], t[1])
 	spark = SparkSession.builder \
-			.appName('RLab_Stats_Report___Prepare_Online_Data') \
+			.appName('RLab_Stats_Report___Cal_Online_Stats') \
 			.config(conf=sparkConf) \
 			.enableHiveSupport() \
 			.getOrCreate()
@@ -55,54 +62,41 @@ if __name__ == '__main__':
 
 	print('====> Parsing local arguments')
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--query_date', type=str)
-	parser.add_argument('--forward', type=int)
+	parser.add_argument('--fr', type=str)
+	parser.add_argument('--to', type=str)
+	parser.add_argument('--os', choices=['a', 'i'])
 	args = parser.parse_args()
-	to = datetime.strptime(args.query_date, '%Y%m%d').date()+timedelta(days=args.forward)
-	to = to.strftime('%Y%m%d')
+	assert args.fr <= args.to
 
 	print('====> Start calculation')
 	result = {}
-	uids = retrieveUidInfo(spark, to).repartition(5000, ['uid'])
-	records = retrieveOnlineRecords(spark, args.query_date, to).repartition(5000, ['uid'])
+	uids = retrieveUidInfo(spark, args.to, args.os)
+	records = retrieveOnlineRecords(spark, args.fr, args.to, args.os)
 	records = records.join(uids, on=['uid'], how='left_outer').cache()
 
-	devices = records.groupBy(['imei', 'os']).agg(F.count(F.lit(1)).alias('device_online_times'), \
-		F.countDistinct('app_package').alias('device_online_app_count')).cache()
-	devices_stats = devices.where(devices.os == 'a').select(F.count(F.lit(1)).alias('android_online_device_count'), \
-		F.mean('device_online_times').alias('android_avg_online_times_pd'), \
-		F.mean('device_online_app_count').alias('android_avg_online_app_pd')).collect()
-	result['android_online_device_count'] = devices_stats[0]['android_online_device_count']
-	result['android_avg_online_times_pd'] = devices_stats[0]['android_avg_online_times_pd']
-	result['android_avg_online_app_pd'] = devices_stats[0]['android_avg_online_app_pd']
-	'''
-	devices_stats = devices.where(devices.os == 'i').select(F.count(F.lit(1)).alias('ios_online_device_count'), \
-		F.mean('device_online_times').alias('ios_avg_online_times_pd'), \
-		F.mean('device_online_app_count').alias('ios_avg_online_app_pd')).collect()
-	result['ios_online_device_count'] = devices_stats[0]['ios_online_device_count']
-	result['ios_avg_online_times_pd'] = devices_stats[0]['ios_avg_online_times_pd']
-	result['ios_avg_online_app_pd'] = devices_stats[0]['ios_avg_online_app_pd']
-	'''
-	devices.unpersist()
+	devices = records.repartition(5000, ['device_id']).groupBy(['device_id']).agg(\
+		F.count(F.lit(1)).alias('device_online_times'), \
+		F.approx_count_distinct('app_package', rsd=0.05).alias('device_online_app_count'))
+	devices = devices.select(\
+		F.count(F.lit(1)).alias('online_device_count'), \
+		F.mean('device_online_times').alias('avg_online_times_per_device'), \
+		F.mean('device_online_app_count').alias('avg_online_app_per_device')).collect()
+	#print(devices)
+	result['online_device_count'] = devices[0]['online_device_count']
+	result['avg_online_times_per_device'] = devices[0]['avg_online_times_per_device']
+	result['avg_online_app_per_device'] = devices[0]['avg_online_app_per_device']
 
-	apps = records.groupBy(['app_package', 'os']).agg(F.count(F.lit(1)).alias('app_online_times'), \
-		F.countDistinct('imei').alias('app_online_device_count')).cache()
-	apps_stats = apps.where(apps.os == 'a').select(F.count(F.lit(1)).alias('android_online_app_count'), \
-		F.mean('app_online_times').alias('android_avg_online_times_pa'), \
-		F.mean('app_online_device_count').alias('android_avg_online_device_pa')).collect()
-	result['android_online_app_count'] = apps_stats[0]['android_online_app_count']
-	result['android_avg_online_times_pa'] = apps_stats[0]['android_avg_online_times_pa']
-	result['android_avg_online_device_pa'] = apps_stats[0]['android_avg_online_device_pa']
-	'''
-	apps_stats = apps.where(apps.os == 'i').select(F.count(F.lit(1)).alias('ios_online_app_count'), \
-		F.mean('app_online_times').alias('ios_avg_online_times_pa'), \
-		F.mean('app_online_device_count').alias('ios_avg_online_device_pa')).collect()
-	result['ios_online_app_count'] = apps_stats[0]['ios_online_app_count']
-	result['ios_avg_online_times_pa'] = apps_stats[0]['ios_avg_online_times_pa']
-	result['ios_avg_online_device_pa'] = apps_stats[0]['ios_avg_online_device_pa']
-	'''
-	apps.unpersist()
+	apps = records.repartition(5000, ['app_package']).groupBy(['app_package']).agg(\
+		F.count(F.lit(1)).alias('app_online_times'), \
+		F.approx_count_distinct('device_id', rsd=0.05).alias('app_online_device_count'))
+	apps = apps.select(\
+		F.count(F.lit(1)).alias('online_app_count'), \
+		F.mean('app_online_times').alias('avg_online_times_per_app'), \
+		F.mean('app_online_device_count').alias('avg_online_device_per_app')).collect()
+	#print(apps)
+	result['online_app_count'] = apps_stats[0]['online_app_count']
+	result['avg_online_times_per_app'] = apps_stats[0]['avg_online_times_per_app']
+	result['avg_online_device_per_app'] = apps_stats[0]['avg_online_device_per_app']
 
-	result = spark.createDataFrame([result])
-	result = result.withColumn('data_date', F.lit(args.query_date))
-	result.repartition(1).write.csv('/user/hive/warehouse/ronghui.db/rlab_stats_report/online/{0}'.format(args.query_date), header=True)
+	result = sc.parallelize([result]).map(transform_to_row).toDF()
+	result.repartition(1).write.csv('/user/hive/warehouse/ronghui.db/rlab_stats_report/online/{0}'.format(args.fr), header=True)
