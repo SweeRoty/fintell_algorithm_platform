82820fe76b72e8a81222c9fb7c89784fce1ef6bf
SweeRoty <sweeroty@gmail.com>
2020-06-15 11:45:39 +0800

    add an os parameter, modify itime condition, use approx_count instead of count, create dataframe from rdd and optimize calculation process

--- stats/cal_lbs_stats.py
+++ stats/cal_lbs_stats.py
@@ -9,17 +9,19 @@ from pyspark.sql import SparkSession
 
 import argparse
 
-def retrieveUidInfo(spark, to):
+def retrieveUidInfo(spark, to, os):
+	device = 'imei' if os == 'a' else 'idfa'
 	sql = """
 		select
 			distinct uid,
 			package_name app_package,
-			imei
+			{0} device_id
 		from
-			edw.user_register_log
+			ronghui.register_user_log
 		where
-			data_date <= '{0}'
-	""".format(to)
+			data_date <= '{1}'
+			and platform = '{2}'
+	""".format(device, to, os)
 	print(sql)
 	uids = spark.sql(sql)
 	return uids
@@ -28,17 +30,21 @@ def retrieveLBSRecords(spark, fr, to):
 	sql = """
 		select
 			uid,
-			platform os,
 			coordinate_source type
 		from
 			edw.active_user_log
 		where
 			data_date between '{0}' and '{1}'
-			and itime between unix_timestamp('{0}', 'YYYYmmdd') and unix_timestamp('{1}', 'YYYYmmdd')
+			and from_unixtime(itime, 'yyyyMMdd') between '{0}' and '{1}'
 	""".format(fr, to)
 	print(sql)
 	records = spark.sql(sql)
 	return records
+
+def transform_to_row(row_dict):
+	global args
+	row_dict['data_date'] = args.fr
+	return Row(**row_dict)
 
 if __name__ == '__main__':
 	print('====> Initializing Spark APP')
@@ -48,7 +54,7 @@ if __name__ == '__main__':
 	for t in localConf.items('spark-config'):
 		sparkConf.set(t[0], t[1])
 	spark = SparkSession.builder \
-			.appName('RLab_Stats_Report___Prepare_LBS_Data') \
+			.appName('RLab_Stats_Report___Cal_LBS_Stats') \
 			.config(conf=sparkConf) \
 			.enableHiveSupport() \
 			.getOrCreate()
@@ -56,52 +62,52 @@ if __name__ == '__main__':
 
 	print('====> Parsing local arguments')
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--query_date', type=str)
-	parser.add_argument('--forward', type=int)
+	parser.add_argument('--fr', type=str)
+	parser.add_argument('--to', type=str)
+	parser.add_argument('--os', choices=['a', 'i'])
 	args = parser.parse_args()
-	to = datetime.strptime(args.query_date, '%Y%m%d').date()+timedelta(days=args.forward)
-	to = to.strftime('%Y%m%d')
+	assert args.fr <= args.to
 
 	print('====> Start calculation')
-	types = ['gps', 'wifi', 'cell', 'ip']
+	types = ['GPS', 'WIFI', 'CELL', 'IP']
 	result = {}
-	uids = retrieveUidInfo(spark, to).repartition(5000, ['uid'])
-	records = retrieveLBSRecords(spark, args.query_date, to).repartition(5000, ['uid'])
-	records = records.join(uids, on=['uid'], how='left_outer').where(records.os == 'a').drop('os').cache() # only android devices
+	uids = retrieveUidInfo(spark, args.to, args.os)
+	records = retrieveLBSRecords(spark, args.fr, args.to)
+	records = records.join(uids, on=['uid'], how='inner').cache()
 	result['lbs_point_count'] = records.count()
-	result['lbs_gps_point_count'] = records.where(records.type == 'gps').count()
-	result['lbs_wifi_point_count'] = records.where(records.type == 'wifi').count()
-	result['lbs_cell_point_count'] = records.where(records.type == 'cell').count()
-	result['lbs_ip_point_count'] = records.where(records.type == 'ip').count()
+	result['lbs_gps_point_count'] = records.where(records.type == 'GPS').count()
+	result['lbs_wifi_point_count'] = records.where(records.type == 'WIFI').count()
+	result['lbs_cell_point_count'] = records.where(records.type == 'CELL').count()
+	result['lbs_ip_point_count'] = records.where(records.type == 'IP').count()
 
-	general_stats = records.groupBy(['type']).agg(F.countDistinct('imei').alias('lbs_device_count'), \
-		F.countDistinct('app_package').alias('lbs_app_count')).cache()
+	devices = records.repartition(5000, ['device_id']).groupBy(['device_id', 'type']).agg(\
+		F.count(F.lit(1)).alias('device_lbs_times'), \
+		F.approx_count_distinct('app_package', rsd=0.05).alias('device_lbs_app_count')).cache()
 	for t in types:
-		row = general_stats.where(general_stats.type == t).collect()[0]
-		result['lbs_{0}_device_count'.format(t)] = row['lbs_device_count']
-		result['lbs_{0}_app_count'.format(t)] = row['lbs_app_count']
-	general_stats.unpersist()
-
-	devices = records.groupBy(['imei', 'type']).agg(F.count(F.lit(1)).alias('device_lbs_times'), \
-		F.countDistinct('app_package').alias('device_lbs_app_count'))
-	devices_stats = devices.groupBy(['type']).agg(F.mean('device_lbs_times').alias('avg_lbs_times_pd'), \
-		F.mean('device_lbs_app_count').alias('avg_lbs_app_count_pd')).cache()
-	for t in types:
-		row = devices_stats.where(devices_stats.type == t).collect()[0]
-		result['avg_lbs_times_pd_{0}'.format(t)] = row['avg_lbs_times_pd']
-		result['avg_lbs_app_count_pd_{0}'.format(t)] = row['avg_lbs_app_count_pd']
+		devices_stats = devices.where(devices.type == t).agg(\
+			F.count(F.lit(1)).alias('lbs_device_count_{0}'.format(t)), \
+			F.mean('device_lbs_times').alias('avg_lbs_times_per_device_{0}'.format(t)), \
+			F.mean('device_lbs_app_count').alias('avg_lbs_app_per_device_{0}'.format(t))).collect()
+		print('=========>', t, devices_stats)
+		result['lbs_device_count_{0}'.format(t)] = devices_stats[0]['lbs_device_count_{0}'.format(t)]
+		result['avg_lbs_times_per_device_{0}'.format(t)] = devices_stats[0]['avg_lbs_times_per_device_{0}'.format(t)]
+		result['avg_lbs_app_per_device_{0}'.format(t)] = devices_stats[0]['avg_lbs_app_per_device_{0}'.format(t)]
 	devices.unpersist()
 
-	apps = records.groupBy(['app_package', 'type']).agg(F.count(F.lit(1)).alias('app_lbs_times'), \
-		F.countDistinct('imei').alias('app_lbs_device_count'))
-	apps_stats = apps.groupBy(['type']).agg(F.mean('app_lbs_times').alias('avg_lbs_times_pa'), \
-		F.mean('app_lbs_device_count').alias('avg_lbs_device_count_pa')).cache()
+	apps = records.repartition(5000, ['app_package']).groupBy(['app_package', 'type']).agg(\
+		F.count(F.lit(1)).alias('app_lbs_times'), \
+		F.approx_count_distinct('device_id', rsd=0.05).alias('app_lbs_device_count')).cache()
+	apps.write.csv('/user/hive/warehouse/ronghui.db/rlab_stats_report/lbs/app_rank/{0}'.format(args.fr), header=True)
 	for t in types:
-		row = apps_stats.where(apps_stats.type == t).collect()[0]
-		result['avg_lbs_times_pa_{0}'.format(t)] = row['avg_lbs_times_pa']
-		result['avg_lbs_device_count_pa_{0}'.format(t)] = row['avg_lbs_device_count_pa']
-	apps_stats.unpersist()
+		apps_stats = apps.where(apps.type == t).agg(\
+			F.count(F.lit(1)).alias('lbs_app_count_{0}'.format(t)), \
+			F.mean('app_lbs_times').alias('avg_lbs_times_per_app_{0}'.format(t)), \
+			F.mean('app_lbs_device_count').alias('avg_lbs_device_per_app_{0}'.format(t))).collect()
+		print('=========>', t, apps_stats)
+		result['lbs_app_count_{0}'.format(t)] = devices_stats[0]['lbs_app_count_{0}'.format(t)]
+		result['avg_lbs_times_per_app_{0}'.format(t)] = devices_stats[0]['avg_lbs_times_per_app_{0}'.format(t)]
+		result['avg_lbs_device_per_app_{0}'.format(t)] = devices_stats[0]['avg_lbs_device_per_app_{0}'.format(t)]
+	apps.unpersist()
 	
-	result = spark.createDataFrame([result])
-	result = result.withColumn('data_date', F.lit(args.query_date))
-	result.repartition(1).write.csv('/user/hive/warehouse/ronghui.db/rlab_stats_report/lbs/{0}'.format(args.query_date), header=True)
+	result = sc.parallelize([result]).map(transform_to_row).toDF()
+	result.repartition(1).write.csv('/user/hive/warehouse/ronghui.db/rlab_stats_report/lbs/{0}'.format(args.fr), header=True)
