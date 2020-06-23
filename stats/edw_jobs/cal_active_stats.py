c5c8235f264471437f2fe25b56ad549a786d4375
SweeRoty <sweeroty@gmail.com>
2020-06-23 12:28:08 +0800

    make a edw directory and move related scripts into it

--- stats/edw_jobs/cal_active_stats.py
+++ stats/edw_jobs/cal_active_stats.py
@@ -25,12 +25,12 @@ def retrieveUidInfo(spark, to, os):
 	uids = spark.sql(sql)
 	return uids
 
-def retrieveOnlineRecords(spark, fr, to, os):
+def retrieveActiveRecords(spark, fr, to, os):
 	sql = """
 		select
 			md5(cast(uid as string)) uid
 		from
-			edw.online_user_log
+			edw.active_user_log
 		where
 			data_date between '{0}' and '{1}'
 			and from_unixtime(itime, 'yyyyMMdd') between '{0}' and '{1}'
@@ -48,12 +48,12 @@ def transform_to_row(row_dict):
 if __name__ == '__main__':
 	print('====> Initializing Spark APP')
 	localConf = RawConfigParser()
-	localConf.read('./config')
+	localConf.read('../config')
 	sparkConf = SparkConf()
 	for t in localConf.items('spark-config'):
 		sparkConf.set(t[0], t[1])
 	spark = SparkSession.builder \
-			.appName('RLab_Stats_Report___Cal_Online_Stats') \
+			.appName('RLab_Stats_Report___Cal_Active_Stats') \
 			.config(conf=sparkConf) \
 			.enableHiveSupport() \
 			.getOrCreate()
@@ -71,30 +71,30 @@ if __name__ == '__main__':
 	print('====> Start calculation')
 	result = {}
 	uids = retrieveUidInfo(spark, to, args.os)
-	records = retrieveOnlineRecords(spark, fr, to, args.os)
+	records = retrieveActiveRecords(spark, fr, to, args.os)
 	records = records.join(uids, on=['uid'], how='left_outer').cache()
 
 	devices = records.repartition(5000, ['device_id']).groupBy(['device_id']).agg(\
-		F.count(F.lit(1)).alias('device_online_times'), \
-		F.approx_count_distinct('app_package', rsd=0.05).alias('device_online_app_count'))
+		F.count(F.lit(1)).alias('device_active_times'), \
+		F.approx_count_distinct('app_package', rsd=0.05).alias('device_active_app_count'))
 	devices = devices.select(\
-		F.count(F.lit(1)).alias('online_device_count'), \
-		F.mean('device_online_times').alias('avg_online_times_per_device'), \
-		F.mean('device_online_app_count').alias('avg_online_app_per_device')).collect()
-	result['online_device_count'] = devices[0]['online_device_count']
-	result['avg_online_times_per_device'] = devices[0]['avg_online_times_per_device']
-	result['avg_online_app_per_device'] = devices[0]['avg_online_app_per_device']
+		F.count(F.lit(1)).alias('active_device_count'), \
+		F.mean('device_active_times').alias('avg_active_times_per_device'), \
+		F.mean('device_active_app_count').alias('avg_active_app_per_device')).collect()
+	result['active_device_count'] = devices[0]['active_device_count']
+	result['avg_active_times_per_device'] = devices[0]['avg_active_times_per_device']
+	result['avg_active_app_per_device'] = devices[0]['avg_active_app_per_device']
 
 	apps = records.repartition(1000, ['app_package']).groupBy(['app_package']).agg(\
-		F.count(F.lit(1)).alias('app_online_times'), \
-		F.approx_count_distinct('device_id', rsd=0.05).alias('app_online_device_count'))
+		F.count(F.lit(1)).alias('app_active_times'), \
+		F.approx_count_distinct('device_id', rsd=0.05).alias('app_active_device_count'))
 	apps = apps.select(\
-		F.count(F.lit(1)).alias('online_app_count'), \
-		F.mean('app_online_times').alias('avg_online_times_per_app'), \
-		F.mean('app_online_device_count').alias('avg_online_device_per_app')).collect()
-	result['online_app_count'] = apps[0]['online_app_count']
-	result['avg_online_times_per_app'] = apps[0]['avg_online_times_per_app']
-	result['avg_online_device_per_app'] = apps[0]['avg_online_device_per_app']
+		F.count(F.lit(1)).alias('active_app_count'), \
+		F.mean('app_active_times').alias('avg_active_times_per_app'), \
+		F.mean('app_active_device_count').alias('avg_active_device_per_app')).collect()
+	result['active_app_count'] = apps[0]['active_app_count']
+	result['avg_active_times_per_app'] = apps[0]['avg_active_times_per_app']
+	result['avg_active_device_per_app'] = apps[0]['avg_active_device_per_app']
 
 	result = sc.parallelize([result]).map(transform_to_row).toDF()
-	result.repartition(1).write.csv('/user/hive/warehouse/ronghui.db/rlab_stats_report/online/{0}'.format(args.query_month), header=True)
+	result.repartition(1).write.csv('/user/hive/warehouse/ronghui.db/rlab_stats_report/active/{0}'.format(args.query_month), header=True)
--- stats/edw_jobs/cal_lbs_stats.py
+++ stats/edw_jobs/cal_lbs_stats.py
@@ -49,7 +49,7 @@ def transform_to_row(row_dict):
 if __name__ == '__main__':
 	print('====> Initializing Spark APP')
 	localConf = RawConfigParser()
-	localConf.read('./config')
+	localConf.read('../config')
 	sparkConf = SparkConf()
 	for t in localConf.items('spark-config'):
 		sparkConf.set(t[0], t[1])
