8801b863d2826a076a00e1d80194b2774d95b5c5
SweeRoty <sweeroty@gmail.com>
2020-07-01 17:30:05 +0800

    add a month parameter and calculate ratio for ISP and price devices

--- stats/pre_jobs/cal_device_info.py
+++ stats/pre_jobs/cal_device_info.py
@@ -25,20 +25,39 @@ if __name__ == '__main__':
 	sc.setLogLevel('ERROR')
 
 	print('====> Parsing local arguments')
-	
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--query_month', type=str)
+	args = parser.parse_args()
+
 	print('====> Start calculation')
 	bucketizer = Bucketizer(splits=[0, 500, 1000, 1500, 2000, 2500, 3000, 4000, 5000, float('Inf')], \
 		inputCol='price', outputCol='price_bin')
-	for month in ['202001', '202002', '202003', '202004', '202005']:
-		active_devices = spark.read.csv('hgy/rlab_stats_report/device_info/{0}'.format(month), header=True).cache()
-		android_10_count = active_devices.select(F.col('android_version').cast('int').alias('android_version')).where(\
-			F.col('android_version') == 10).count()
-		print('Android 10 count is: {0}'.format(android_10_count))
-		print('Android 10 ratio is: {0}'.format(round(android_10_count/active_devices.count(), 10)))
-		isp_stats = active_devices.where((active_devices.isp.contains('移动')) | (active_devices.isp.contains('联通')) | (active_devices.isp.contains('电信')))
-		isp_stats = isp_stats.groupBy(['isp']).agg(F.count(F.lit(1)).alias('isp_count')).collect()
-		print(isp_stats)
-		active_devices = active_devices.where(active_devices.price.isNotNull()).select(F.col('price').cast('int').alias('price'))
-		active_devices = bucketizer.setHandleInvalid('keep').transform(active_devices)
-		price_stats = active_devices.groupBy(['price_bin']).agg(F.count(F.lit(1)).alias('price_bin_count')).collect()
-		print(price_stats)
+
+	active_devices = spark.read.csv('hgy/rlab_stats_report/device_info/{0}_1'.format(args.query_month), header=True).cache()
+	n = active_devices.count()
+	print('=========> Total number of active devices is {0}'.format(n))
+
+	android_10_count = active_devices.select(F.col('android_version').cast('int').alias('android_version')).where(\
+		F.col('android_version') == 10).count()
+	print('=========> Android 10 count is: {0}'.format(android_10_count))
+	print('=========> Android 10 ratio is: {0}'.format(round(android_10_count/n, 10)))
+
+	isp_stats = active_devices.where((active_devices.isp.contains('移动')) | (active_devices.isp.contains('联通')) | (active_devices.isp.contains('电信')))
+	isp_stats = isp_stats.groupBy(['isp']).agg(F.count(F.lit(1)).alias('isp_count')).collect()
+	isp_count = 0
+	for row in isp_stats:
+		print('=========> ISP-{0}, Count-{1}'.format(row['isp'].encode('utf-8'), row['isp_count']))
+		isp_count += row['isp_count']
+	print('=========> ISP count is {0}'.format(isp_count))
+	print('=========> ISP ratio is {0}'.format(round(isp_count/n, 4)))
+
+	active_devices = active_devices.where(active_devices.price.isNotNull()).select(F.col('price').cast('int').alias('price'))
+	active_devices = bucketizer.setHandleInvalid('keep').transform(active_devices)
+	price_stats = active_devices.groupBy(['price_bin']).agg(F.count(F.lit(1)).alias('price_bin_count')).collect()
+	price_count = 0
+	for row in price_stats:
+		if row['price_bin'] != 'price_bin_None':
+			print('=========> PriceBin-{0}, Count-{1}'.format(row['price_bin'], row['price_bin_count']))
+			price_count += row['price_bin_count']
+	print('=========> Price count is {0}'.format(price_count))
+	print('=========> Price ratio is {0}'.format(round(price_count/n, 4)))
