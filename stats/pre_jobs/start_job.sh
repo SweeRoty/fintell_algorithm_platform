07707e900dfc3a532efb66a36067f44fc9d6448a
SweeRoty <sweeroty@gmail.com>
2020-07-20 11:49:04 +0800

    rearrange the script lines

--- stats/pre_jobs/start_job.sh
+++ stats/pre_jobs/start_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-query_months=(202001 202002 202003 202004 202005)
+nohup spark-submit prepare_app_category.py &
+query_months=(202001 202002 202003 202004 202005 202006)
 for i in ${!query_months[@]}; 
 do
   nohup spark-submit extract_active_devices.py --query_month ${query_months[$i]} &
   nohup spark-submit prepare_device_info.py --query_month ${query_months[$i]} &
 done
-nohup spark-submit prepare_app_category.py &
